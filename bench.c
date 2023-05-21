2cc6703c13d5e199aba403aa6ff22e15cd26a5a7
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-05-21 13:36:05 +0200

    Don't use structs in times

--- bench.c
+++ bench.c
@@ -51,25 +51,24 @@
 #define PCAST    (void *(*)(void *))
 
 #ifndef _GNU_SOURCE
-#define strdupa(a) strcpy((char*)alloca(strlen(a) + 1), a)
+#define strdupa(a) strcpy((char *)alloca(strlen(a) + 1), a)
 #endif
 
-typedef struct
-{
-    char *filename;
-    char *path;
+typedef struct {
+	char *filename;
+	char *path;
 
-    struct { int yours, cs50; } misspelled;
-    struct { int yours, cs50; } dictionary;
-    struct { int yours, cs50; } text;
+	int misspelled, cs50_misspelled;
+	int dictionary, cs50_dictionary;
+	int text, cs50_text;
 
-    struct { float yours, cs50; } load;
-    struct { float yours, cs50; } check;
-    struct { float yours, cs50; } size;
-    struct { float yours, cs50; } unload;
-    struct { float yours, cs50; } total;
+	float load, cs50_load;
+	float check, cs50_check;
+	float size, cs50_size;
+	float unload, cs50_unload;
+	float total, cs50_total;
 
-    bool  success;
+	bool success;
 } record;
 
 static void  run_benchmark(record *mrecord);
@@ -83,284 +82,263 @@ static char *cs50_speller = CS50_SPELLER;
 
 
 int main(int argc, char *argv[]) {
-    bool multithreading = true;
-    int arg = 0;
-    while ((arg = getopt(argc, argv, "tms:")) != -1) {
-        if (arg == 't')
-            includeStaff = true;
-        else if (arg == 'm')
-            multithreading = false;
-        else if (arg == 's') {
-            cs50_speller = optarg;
-            includeStaff = true;
-        }
-    }
+	bool multithreading = true;
+	int  arg            = 0;
+	while ((arg = getopt(argc, argv, "tms:")) != -1) {
+		if (arg == 't')
+			includeStaff = true;
+		else if (arg == 'm')
+			multithreading = false;
+		else if (arg == 's') {
+			cs50_speller = optarg;
+			includeStaff = true;
+		}
+	}
 
-    // make sure speller program exists
-    if (access("speller", X_OK) == -1)
-        error_m("speller binary does not exist, please compile it and place it "
-                "in the current directory\n",
-                1);
+	// make sure speller program exists
+	if (access("speller", X_OK) == -1)
+		error_m("speller binary does not exist, please compile it and place it "
+		        "in the current directory\n",
+		        1);
 
-    if (includeStaff && access(cs50_speller, X_OK) == -1)
-        error_m("Staff speller cannot be opened\n", 1);
+	if (includeStaff && access(cs50_speller, X_OK) == -1)
+		error_m("Staff speller cannot be opened\n", 1);
 
-    // setup records to benchmark
-    record records[MAX_FILES];
+	// setup records to benchmark
+	record records[MAX_FILES];
 
-    // If no staff solution, then cs50 values would be left uninitialized
-    memset(records, 0, sizeof records);
+	// If no staff solution, then cs50 values would be left uninitialized
+	memset(records, 0, sizeof records);
 
-    int num_records = 0;
+	int num_records = 0;
 
-    DIR *dirp = opendir(CS50_TEXTS);
-    if (!dirp)
-        error_m("Could not read " CS50_TEXTS "\n", 1);
+	DIR *dirp = opendir(CS50_TEXTS);
+	if (!dirp) error_m("Could not read " CS50_TEXTS "\n", 1);
 
-    for (struct dirent *dir = NULL; (dir = readdir(dirp)) != NULL;) {
-        if (dir->d_name[0] == '.') continue;
+	for (struct dirent *dir = NULL; (dir = readdir(dirp)) != NULL;) {
+		if (dir->d_name[0] == '.') continue;
 
-        records[num_records].filename = strdupa(dir->d_name);
+		records[num_records].filename = strdupa(dir->d_name);
 
-        char path[512];
-        strcpy(path, CS50_TEXTS "/");
-        strcat(path, dir->d_name);
-        records[num_records++].path = strdupa(path);
-    }
+		char path[512];
+		strcpy(path, CS50_TEXTS "/");
+		strcat(path, dir->d_name);
+		records[num_records++].path = strdupa(path);
+	}
 
-    closedir(dirp);
+	closedir(dirp);
 
-    // clear screen
-    printf(C_CLEAR);
+	// clear screen
+	printf(C_CLEAR);
 
-    // print header
-    printf("\t" C_CS50  "Cyan   " C_RESET "- CS50's implementation\n");
-    printf("\t" C_YOURS "Yellow " C_RESET "- your implementation\n");
-    printf("\t" C_BOLD  "Bold   " C_RESET "- lesser time\n");
-
-
-    //printf("\n\tRecord struct size: %zu\n", sizeof(record));
-
-    printf("\n");
-    printf("%16.16s ", "Filename");
-    printf("Status\t");
-    printf("Load\t\t");
-    printf("Check\t\t");
-    printf("Size\t\t");
-    printf("Unload\t\t");
-    printf("Total\t\t");
-    printf("\n");
-
-    float ytload = 0, ytcheck = 0, ytsize = 0, ytunload = 0, yttotal = 0;
-    float ctload = 0, ctcheck = 0, ctsize = 0, ctunload = 0, cttotal = 0;
+	// print header
+	printf("\t" C_CS50 "Cyan   " C_RESET "- CS50's implementation\n");
+	printf("\t" C_YOURS "Yellow " C_RESET "- your implementation\n");
+	printf("\t" C_BOLD "Bold   " C_RESET "- lesser time\n");
 
 
-    // benchmark the files and output records
-    pthread_t threads[num_records]; // one thread per benchmark
-    if (multithreading)
-    {
-        for (int i = 0; i < num_records; i++)
-        {
-            pthread_create(&threads[i], NULL, PCAST run_benchmark, &records[i]);
-        }
-    }
+	// printf("\n\tRecord struct size: %zu\n", sizeof(record));
 
-    for (int i = 0; i < num_records; i++)
-    {
+	printf("\n");
+	printf("%16.16s ", "Filename");
+	printf("Status\t");
+	printf("Load\t\t");
+	printf("Check\t\t");
+	printf("Size\t\t");
+	printf("Unload\t\t");
+	printf("Total\t\t");
+	printf("\n");
 
-        if (multithreading)
-            pthread_join(threads[i], NULL);
-        else
-            run_benchmark(&records[i]);
-
-        // print filename and flush buffer
-        printf("%16.16s ", records[i].filename);
-
-        // print status
-        if (records[i].success)
-            printf(C_BOLD C_GREEN "OK\t");
-        else
-            printf(C_BOLD C_RED "ERROR\t");
-        printf(C_RESET);
-
-        // print data
-        print_row(&records[i]);
-
-        // keep track of totals
-        ctload   += records[i].load.cs50;
-        ytload   += records[i].load.yours;
-
-        ctcheck  += records[i].check.cs50;
-        ytcheck  += records[i].check.yours;
-
-        ctsize   += records[i].size.cs50;
-        ytsize   += records[i].size.yours;
-
-        ctunload += records[i].unload.cs50;
-        ytunload += records[i].unload.yours;
-
-        cttotal  += records[i].total.cs50;
-        yttotal  += records[i].total.yours;
-    }
-
-    // display averages
-    printf("\n");
-    printf("%15.15s \t", "Average:");
-
-    print_row(&(record){
-        .load.yours   = ytload   / num_records,
-        .load.cs50    = ctload   / num_records,
-        .check.yours  = ytcheck  / num_records,
-        .check.cs50   = ctcheck  / num_records,
-        .size.yours   = ytsize   / num_records,
-        .size.cs50    = ctsize   / num_records,
-        .unload.yours = ytunload / num_records,
-        .unload.cs50  = ctunload / num_records,
-        .total.yours  = yttotal  / num_records,
-        .total.cs50   = cttotal  / num_records
-    });
-} // main
+	float ytload = 0, ytcheck = 0, ytsize = 0, ytunload = 0, yttotal = 0;
+	float ctload = 0, ctcheck = 0, ctsize = 0, ctunload = 0, cttotal = 0;
 
 
-static void run_benchmark(record *mrecord)
-{
-    char user_command[250];
-    char cs50_command[250];
+	// benchmark the files and output records
+	pthread_t threads[num_records]; // one thread per benchmark
+	if (multithreading) {
+		for (int i = 0; i < num_records; i++) {
+			pthread_create(&threads[i], NULL, PCAST run_benchmark, &records[i]);
+		}
+	}
 
-    sprintf(user_command, "./speller %s", mrecord->path);
-    sprintf(cs50_command, "%s %s", cs50_speller, mrecord->path);
+	for (int i = 0; i < num_records; i++) {
 
-    // execute and parse user results
-    FILE *fpipe = popen(user_command, "r");
-    if (!fpipe)
-    {
-        mrecord->success = false;
-        return;
-    }
-    mrecord->success = true;
+		if (multithreading)
+			pthread_join(threads[i], NULL);
+		else
+			run_benchmark(&records[i]);
 
-    char buffer[200];
+		// print filename and flush buffer
+		printf("%16.16s ", records[i].filename);
 
-    // consume till stats
-    while (fgets(buffer, sizeof(buffer), fpipe))
-        if (strstr(buffer, "WORDS MISSPELLED") != NULL)
-            break;
+		// print status
+		if (records[i].success)
+			printf(C_BOLD C_GREEN "OK\t");
+		else
+			printf(C_BOLD C_RED "ERROR\t");
+		printf(C_RESET);
 
-    sscanf(buffer, "WORDS MISSPELLED: %d\n", &mrecord->misspelled.yours);
+		// print data
+		print_row(&records[i]);
 
-    // parse output
-    fscanf(fpipe,   "WORDS IN DICTIONARY:  %d\n"
-                    "WORDS IN TEXT:        %d\n"
-                    "TIME IN load:         %f\n"
-                    "TIME IN check:        %f\n"
-                    "TIME IN size:         %f\n"
-                    "TIME IN unload:       %f\n"
-                    "TIME IN TOTAL:        %f\n"
-            , &mrecord->dictionary.yours
-            , &mrecord->text.yours
-            , &mrecord->load.yours
-            , &mrecord->check.yours
-            , &mrecord->size.yours
-            , &mrecord->unload.yours
-            , &mrecord->total.yours
-    );
+		// keep track of totals
+		ctload += records[i].cs50_load;
+		ytload += records[i].load;
 
-    pclose(fpipe);
+		ctcheck += records[i].cs50_check;
+		ytcheck += records[i].check;
 
-    if (!includeStaff)
-        return;
+		ctsize += records[i].cs50_size;
+		ytsize += records[i].size;
 
-    // execute and parse cs50 results
-    fpipe = popen(cs50_command, "r");
-    if (!fpipe)
-    {
-        mrecord->success = false;
-        return;
-    }
+		ctunload += records[i].cs50_unload;
+		ytunload += records[i].unload;
 
-    // consume till stats
-    while (fgets(buffer, sizeof(buffer), fpipe))
-        if (strstr(buffer, "WORDS MISSPELLED") != NULL)
-            break;
+		cttotal += records[i].cs50_total;
+		yttotal += records[i].total;
+	}
 
-    sscanf(buffer, "WORDS MISSPELLED: %d\n", &mrecord->misspelled.cs50);
+	// display averages
+	printf("\n");
+	printf("%15.15s \t", "Average:");
 
-    // parse output
-    fscanf(fpipe,   "WORDS IN DICTIONARY:  %d\n"
-                    "WORDS IN TEXT:        %d\n"
-                    "TIME IN load:         %f\n"
-                    "TIME IN check:        %f\n"
-                    "TIME IN size:         %f\n"
-                    "TIME IN unload:       %f\n"
-                    "TIME IN TOTAL:        %f\n"
+	print_row(&(record){.load        = ytload / num_records,
+	                    .cs50_load   = ctload / num_records,
+	                    .check       = ytcheck / num_records,
+	                    .cs50_check  = ctcheck / num_records,
+	                    .size        = ytsize / num_records,
+	                    .cs50_size   = ctsize / num_records,
+	                    .unload      = ytunload / num_records,
+	                    .cs50_unload = ctunload / num_records,
+	                    .total       = yttotal / num_records,
+	                    .cs50_total  = cttotal / num_records});
 
-            , &mrecord->dictionary.cs50
-            , &mrecord->text.cs50
-            , &mrecord->load.cs50
-            , &mrecord->check.cs50
-            , &mrecord->size.cs50
-            , &mrecord->unload.cs50
-            , &mrecord->total.cs50);
+	return 0;
+}
 
-    pclose(fpipe);
+
+static void run_benchmark(record *mrecord) {
+	char user_command[250];
+	char cs50_command[250];
+
+	sprintf(user_command, "./speller %s", mrecord->path);
+	sprintf(cs50_command, "%s %s", cs50_speller, mrecord->path);
+
+	// execute and parse user results
+	FILE *fpipe = popen(user_command, "r");
+	if (!fpipe) {
+		mrecord->success = false;
+		return;
+	}
+	mrecord->success = true;
+
+	char buffer[200];
+
+	// consume till stats
+	while (fgets(buffer, sizeof(buffer), fpipe))
+		if (strstr(buffer, "WORDS MISSPELLED") != NULL) break;
+
+	sscanf(buffer, "WORDS MISSPELLED: %d\n", &mrecord->misspelled);
+
+	// parse output
+	fscanf(fpipe,
+	       "WORDS IN DICTIONARY:  %d\n"
+	       "WORDS IN TEXT:        %d\n"
+	       "TIME IN load:         %f\n"
+	       "TIME IN check:        %f\n"
+	       "TIME IN size:         %f\n"
+	       "TIME IN unload:       %f\n"
+	       "TIME IN TOTAL:        %f\n",
+	       &mrecord->dictionary, &mrecord->text, &mrecord->load, &mrecord->check,
+	       &mrecord->size, &mrecord->unload, &mrecord->total);
+
+	pclose(fpipe);
+
+	if (!includeStaff) return;
+
+	// execute and parse cs50 results
+	fpipe = popen(cs50_command, "r");
+	if (!fpipe) {
+		mrecord->success = false;
+		return;
+	}
+
+	// consume till stats
+	while (fgets(buffer, sizeof(buffer), fpipe))
+		if (strstr(buffer, "WORDS MISSPELLED") != NULL) break;
+
+	sscanf(buffer, "WORDS MISSPELLED: %d\n", &mrecord->cs50_misspelled);
+
+	// parse output
+	fscanf(fpipe,
+	       "WORDS IN DICTIONARY:  %d\n"
+	       "WORDS IN TEXT:        %d\n"
+	       "TIME IN load:         %f\n"
+	       "TIME IN check:        %f\n"
+	       "TIME IN size:         %f\n"
+	       "TIME IN unload:       %f\n"
+	       "TIME IN TOTAL:        %f\n"
+
+	       ,
+	       &mrecord->cs50_dictionary, &mrecord->cs50_text, &mrecord->cs50_load,
+	       &mrecord->cs50_check, &mrecord->cs50_size, &mrecord->cs50_unload,
+	       &mrecord->cs50_total);
+
+	pclose(fpipe);
 }
 
 
 /**
  * @brief displays the error and exists with the errno
  */
-static void error_m(char *error, int errno)
-{
-    fprintf(stderr, "Error: %s", error);
-    exit(errno);
+static void error_m(char *error, int errno) {
+	fprintf(stderr, "Error: %s", error);
+	exit(errno);
 }
 
 //! @param rec record to print
 //! @param prec how many decimals to print for each value
-static void print_row(record const *rec)
-{
-    printf("%s", compare_times(rec->load.cs50, rec->load.yours));
-    printf(C_CS50 "%.*f\t" C_RESET, 3, rec->load.cs50);
-    printf("%s", compare_times(rec->load.yours, rec->load.cs50));
-    printf(C_YOURS "%.*f\t" C_RESET, 3, rec->load.yours);
+static void print_row(record const *rec) {
+	printf("%s", compare_times(rec->cs50_load, rec->load));
+	printf(C_CS50 "%.*f\t" C_RESET, 3, rec->cs50_load);
+	printf("%s", compare_times(rec->load, rec->cs50_load));
+	printf(C_YOURS "%.*f\t" C_RESET, 3, rec->load);
 
-    printf("%s", compare_times(rec->check.cs50, rec->check.yours));
-    printf(C_CS50 "%.*f\t" C_RESET, 3, rec->check.cs50);
-    printf("%s", compare_times(rec->check.yours, rec->check.cs50));
-    printf(C_YOURS "%.*f\t" C_RESET, 3, rec->check.yours);
+	printf("%s", compare_times(rec->cs50_check, rec->check));
+	printf(C_CS50 "%.*f\t" C_RESET, 3, rec->cs50_check);
+	printf("%s", compare_times(rec->check, rec->cs50_check));
+	printf(C_YOURS "%.*f\t" C_RESET, 3, rec->check);
 
-    printf("%s", compare_times(rec->size.cs50, rec->size.yours));
-    printf(C_CS50 "%.*f\t" C_RESET, 3, rec->size.cs50);
-    printf("%s", compare_times(rec->size.yours, rec->size.cs50));
-    printf(C_YOURS "%.*f\t" C_RESET, 3, rec->size.yours);
+	printf("%s", compare_times(rec->cs50_size, rec->size));
+	printf(C_CS50 "%.*f\t" C_RESET, 3, rec->cs50_size);
+	printf("%s", compare_times(rec->size, rec->cs50_size));
+	printf(C_YOURS "%.*f\t" C_RESET, 3, rec->size);
 
-    printf("%s", compare_times(rec->unload.cs50, rec->unload.yours));
-    printf(C_CS50 "%.*f\t" C_RESET, 3, rec->unload.cs50);
-    printf("%s", compare_times(rec->unload.yours, rec->unload.cs50));
-    printf(C_YOURS "%.*f\t" C_RESET, 3, rec->unload.yours);
+	printf("%s", compare_times(rec->cs50_unload, rec->unload));
+	printf(C_CS50 "%.*f\t" C_RESET, 3, rec->cs50_unload);
+	printf("%s", compare_times(rec->unload, rec->cs50_unload));
+	printf(C_YOURS "%.*f\t" C_RESET, 3, rec->unload);
 
-    printf("%s", compare_times(rec->total.cs50, rec->total.yours));
-    printf(C_CS50 "%.*f\t" C_RESET, 3, rec->total.cs50);
-    printf("%s", compare_times(rec->total.yours, rec->total.cs50));
-    printf(C_YOURS "%.*f\t" C_RESET, 3, rec->total.yours);
+	printf("%s", compare_times(rec->cs50_total, rec->total));
+	printf(C_CS50 "%.*f\t" C_RESET, 3, rec->cs50_total);
+	printf("%s", compare_times(rec->total, rec->cs50_total));
+	printf(C_YOURS "%.*f\t" C_RESET, 3, rec->total);
 
-    printf("\n");
+	printf("\n");
 }
 
 /**
  * @returns string constant bold if num1 is less
  */
-static char *compare_times(float num1, float num2)
-{
-    float const epsilon = 0.0001F;
+static char *compare_times(float num1, float num2) {
+	const float epsilon = 0.0001F;
 
-    // no staff solution or just small diff
-    if (fminf(num1, num2) < epsilon || fabsf(num1 - num2) <= epsilon)
-        return "";
+	// no staff solution or just small diff
+	if (fminf(num1, num2) < epsilon || fabsf(num1 - num2) <= epsilon)
+		return "";
 
-    if (num1 < num2)
-        return C_BOLD;
+	if (num1 < num2) return C_BOLD;
 
-    // num2 greater
-    return "";
+	// num2 greater
+	return "";
 }
