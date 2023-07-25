218d02e2ff0fa7196a45a7fcb58e842c385741b8
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-07-25 11:36:02 +0200

    Add vla warning

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -3,19 +3,20 @@
 # https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
 function(enable_warnings)
-   option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" FALSE)
+	option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" FALSE)
 
-   set(CLANG_WARNINGS
-      -Wall
-      -Wextra
-      -Wshadow
-   )
+	set(CLANG_WARNINGS
+		-Wall
+		-Wextra
+		-Wshadow
+		-Wvla
+	)
 
-   if(WARNINGS_AS_ERRORS)
-      set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
-   endif()
-   
-   if(NOT WIN32)
-      add_compile_options(${CLANG_WARNINGS})
-   endif()
+	if(WARNINGS_AS_ERRORS)
+		set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+	endif()
+
+	if(NOT WIN32)
+		add_compile_options(${CLANG_WARNINGS})
+	endif()
 endfunction()
