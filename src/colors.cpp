b959c0738aa27767b5ef5be4332c30a4eb24a1c7
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-08-09 19:05:53 +0200

    Will removing auto help?

--- src/colors.cpp
+++ src/colors.cpp
@@ -2,5 +2,5 @@
 
 #include <fmt/color.h>
 
-auto const cs50_color = fg(fmt::color::dark_cyan);
-auto const your_color = fg(fmt::color::ghost_white);
+fmt::text_style const cs50_color = fg(fmt::color::dark_cyan);
+fmt::text_style const your_color = fg(fmt::color::ghost_white);
