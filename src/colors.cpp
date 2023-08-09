0e811b6360165ec8978f97e28ea2f965510e38aa
Johannes Eckhoff <j.eckhoff@gmail.com>
2023-08-09 12:12:32 +0200

    Change colors

--- src/colors.cpp
+++ src/colors.cpp
@@ -2,5 +2,5 @@
 
 #include <fmt/color.h>
 
-auto const cs50_color = fg(fmt::color::cyan);
-auto const your_color = fg(fmt::color::white);
+auto const cs50_color = fg(fmt::color::dark_cyan);
+auto const your_color = fg(fmt::color::ghost_white);
