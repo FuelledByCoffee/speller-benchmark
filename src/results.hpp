a76a09e3668e0af069f4f129a3e1d34d7fc380bd
Johannes Eckhoff <j.eckhoff@gmail.com>
2025-04-23 14:15:15 +0200

    Can't change active union member in a constant expression
    So the fmt fg function is changing a union somewhere

--- src/results.hpp
+++ src/results.hpp
@@ -5,7 +5,7 @@
 // output constants
 #define C_CLEAR "\033[2J\033[1;1H"
 
-constexpr inline auto cs50_color = fg(fmt::color::dark_cyan);
-constexpr inline auto your_color = fg(fmt::color::ghost_white);
+const inline auto cs50_color = fg(fmt::color::dark_cyan);
+const inline auto your_color = fg(fmt::color::ghost_white);
 
 void print_results_header();
