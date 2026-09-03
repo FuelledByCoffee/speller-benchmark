e819f679136d69be853a9045318be893e4405dc5
Johannes Eckhoff <j.eckhoff@gmail.com>
2026-09-03 09:26:55 +0200

    style: formatting

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -6,123 +6,123 @@
 #include <ostream>
 
 struct record {
-		void run(std::filesystem::path const &speller,
-		         std::filesystem::path const &txtfile);
+	void run(std::filesystem::path const &speller,
+	         std::filesystem::path const &txtfile);
 
-		int misspelled{};
-		int dictionary{};
-		int text{};
+	int misspelled{};
+	int dictionary{};
+	int text{};
 
-		float load{};
-		float check{};
-		float size{};
-		float unload{};
-		float total{};
+	float load{};
+	float check{};
+	float size{};
+	float unload{};
+	float total{};
 
-		bool success = true;
+	bool success = true;
 
-		constexpr auto operator+=(record const &other) -> record & {
-				load   += other.load;
-				check  += other.check;
-				size   += other.size;
-				unload += other.unload;
-				total  += other.total;
+	constexpr auto operator+=(record const &other) -> record & {
+		load   += other.load;
+		check  += other.check;
+		size   += other.size;
+		unload += other.unload;
+		total  += other.total;
 
-				return *this;
-		}
+		return *this;
+	}
 
-		constexpr auto operator+(record other) const -> record {
-				return other += *this;
-		}
+	constexpr auto operator+(record other) const -> record {
+		return other += *this;
+	}
 
-		template <typename Int = int>
-		constexpr auto operator/=(Int divisor) -> record & {
-				auto div  = static_cast<float>(divisor);
-				load     /= div;
-				check    /= div;
-				size     /= div;
-				unload   /= div;
-				total    /= div;
+	template <typename Int = int>
+	constexpr auto operator/=(Int divisor) -> record & {
+		auto div  = static_cast<float>(divisor);
+		load     /= div;
+		check    /= div;
+		size     /= div;
+		unload   /= div;
+		total    /= div;
 
-				return *this;
-		}
+		return *this;
+	}
 };
 
 class benchmark {
-public:
-		benchmark() = default;
+	public:
+	benchmark() = default;
 
-		benchmark(std::filesystem::path const &txtfile, bool bench_staff,
-		          std::filesystem::path const &staff_executable = "speller50",
-		          std::filesystem::path const &your_executable  = "speller") //
-				: txt(txtfile), m_staff(bench_staff),
-					m_staff_executable(staff_executable),
-					m_your_executable(your_executable) {}
+	benchmark(std::filesystem::path const &txtfile, bool bench_staff,
+	          std::filesystem::path const &staff_executable,
+	          std::filesystem::path const &your_executable) //
+			: txt(txtfile), m_staff(bench_staff),
+				m_staff_executable(staff_executable),
+				m_your_executable(your_executable) {}
 
-		template <typename F>
-		void run(F &&callback) {
-				if (m_staff) cs50.run(m_staff_executable, txt);
-				yours.run(m_your_executable, txt);
-				callback(*this);
-		}
+	template <typename F>
+	void run(F &&callback) {
+		if (m_staff) cs50.run(m_staff_executable, txt);
+		yours.run(m_your_executable, txt);
+		callback(*this);
+	}
 
-		friend auto operator<(benchmark const &left, benchmark const &right) {
-				return left.yours.total < right.yours.total;
-		}
+	friend auto operator<(benchmark const &left, benchmark const &right) {
+		return left.yours.total < right.yours.total;
+	}
 
-		friend auto operator>(benchmark const &left, benchmark const &right) {
-				return right < left;
-		}
+	friend auto operator>(benchmark const &left, benchmark const &right) {
+		return right < left;
+	}
 
-		friend auto operator<=(benchmark const &left, benchmark const &right) {
-				return !(right < left);
-		}
+	friend auto operator<=(benchmark const &left, benchmark const &right) {
+		return !(right < left);
+	}
 
-		friend auto operator>=(benchmark const &left, benchmark const &right) {
-				return !(right > left);
-		}
+	friend auto operator>=(benchmark const &left, benchmark const &right) {
+		return !(right > left);
+	}
 
-		friend auto operator==(benchmark const &left, benchmark const &right) {
-				return left.yours.total == right.yours.total;
-		}
+	friend auto operator==(benchmark const &left, benchmark const &right) {
+		return left.yours.total == right.yours.total;
+	}
 
-		friend auto operator!=(benchmark const &left, benchmark const &right) {
-				return !(left == right);
-		}
+	friend auto operator!=(benchmark const &left, benchmark const &right) {
+		return !(left == right);
+	}
 
-		auto operator+=(benchmark const &other) -> benchmark & {
-				yours += other.yours;
-				cs50  += other.cs50;
-				return *this;
-		}
+	auto operator+=(benchmark const &other) -> benchmark & {
+		yours += other.yours;
+		cs50  += other.cs50;
+		return *this;
+	}
 
-		auto operator+(benchmark rhs) const -> benchmark { return rhs += *this; }
+	auto operator+(benchmark rhs) const -> benchmark { return rhs += *this; }
 
-		template <typename Int = int>
-		auto operator/=(Int divisor) -> benchmark & {
-				yours /= divisor;
-				cs50  /= divisor;
+	template <typename Int = int>
+	auto operator/=(Int divisor) -> benchmark & {
+		yours /= divisor;
+		cs50  /= divisor;
 
-				return *this;
-		}
+		return *this;
+	}
 
-		template <typename Int = int>
-		friend auto operator/(benchmark b, Int divisor) -> benchmark {
-				return b /= divisor;
-		}
+	template <typename Int = int>
+	friend auto operator/(benchmark b, Int divisor) -> benchmark {
+		return b /= divisor;
+	}
 
-		friend auto operator<<(std::ostream &os, benchmark const &results)
-					-> std::ostream &;
+	friend auto operator<<(std::ostream &os, benchmark const &results)
+				-> std::ostream &;
 
-		std::filesystem::path txt;
+	std::filesystem::path txt;
 
-private:
-		record yours{};
-		record cs50{};
+	private:
+	record yours{};
+	record cs50{};
 
-		bool                  m_staff            = false;
-		std::filesystem::path m_staff_executable = "speller50";
-		std::filesystem::path m_your_executable  = "speller";
+	bool                  m_staff            = false;
+	std::filesystem::path m_staff_executable = "speller50";
+	std::filesystem::path m_your_executable  = "speller";
 };
 
 template <>
